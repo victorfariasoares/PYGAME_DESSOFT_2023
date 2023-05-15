fc35d74ede354f741a0c170dcddd49c31a5bca04
cauepda <cauepda@al.insper.edu.br>
2023-05-15 08:12:02 -0300

    primeiras construções

---
+++ logica.py
@@ -0,0 +1,7 @@
+'''
+1 --> fazer tela inicial:
+fundo preto com botão de ação para começar o jogo
+
+
+
+'''
---
+++ tela_inicial.py
@@ -0,0 +1,42 @@
+import pygame
+import random
+from os import path
+
+from config import IMG_DIR, BLACK, FPS, GAME, QUIT, WIDTH, HEIGHT
+
+# ----- Gera tela principal
+
+def init_screen(screen):
+    # Variável para o ajuste de velocidade
+    clock = pygame.time.Clock()
+
+    # Carrega o fundo da tela inicial
+    background = pygame.image.load(path.join(IMG_DIR, 'inicio.png')).convert()
+    background_rect = background.get_rect()
+    pygame.display.set_caption('Jogo do Cauê')
+
+    running = True
+    while running:
+
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False
+
+            if event.type == pygame.KEYUP:
+                state = GAME
+                running = False
+
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+
+    return state
