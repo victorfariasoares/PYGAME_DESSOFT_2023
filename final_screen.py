cc28412642dd3e594ca28bb8eb9e5fd6f42503c2
GiovanaNery <130416807+GiovanaNery@users.noreply.github.com>
2023-05-17 09:23:29 -0300

    imagem final do jogo

--- final_screen.py
+++ final_screen.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os 
 from os import path
 
 from config import IMG_DIR, BLACK, FPS, GAME, QUIT, WIDTH, HEIGHT, RED, WHITE
@@ -10,7 +11,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flappy')
 
 # Carrega o fundo da tela inicial
-background = pygame.image.load(path.join(IMG_DIR, 'img_final_screen_game_over.jpg')).convert()
+background = pygame.image.load(path.join(IMG_DIR, 'img_final_jubilado_again.jpeg')).convert()
 background_rect = background.get_rect()
 
 # Carrega o yes
@@ -20,19 +21,14 @@ yes_button = pygame.image.load(path.join(IMG_DIR, 'final_screen_yes_button.jpg')
 no_button = pygame.image.load(path.join(IMG_DIR, 'final_screen_no_button.jpg')).convert()
 
 background = pygame.transform.scale(background, (480, 600))
-start_button = pygame.transform.scale(no_button, (40, 40))
+no_button = pygame.transform.scale(no_button, (326//3, 175//3))
+yes_button = pygame.transform.scale(yes_button, (326//3, 175//3))
 
 # ----- Inicia assets-------
 tamanho_fonte_1 = 70
-nome_fonte = 'PressStart'
-font_1 = pygame.font.SysFont(nome_fonte, tamanho_fonte_1)
+font_1 = pygame.font.SysFont(None, tamanho_fonte_1)
 frase_1 = 'FLAPPY INSPER'
 text_intro = font_1.render(frase_1, True, (RED))
-
-tamanho_fonte_2 = 25
-font_2 = pygame.font.SysFont(None, tamanho_fonte_2)
-frase_2 = 'CHEGUE AO FINAL DO CURSO DE ENGENHARIA'
-text_descrip = font_2.render(frase_2, True, (WHITE))
 
 #==== obtendo altura e largura dos frames ========
 text_intro_width = text_intro.get_width()
@@ -45,9 +41,6 @@ centro_flappy = WIDTH // 2 - flappy_width // 2
 start_bottom_width = no_button.get_width()
 start_bottom_height = no_button.get_height()
 centro_start = WIDTH // 2 - start_bottom_width // 2
-
-text_descrip_width = text_descrip.get_width()
-centro_text_descrip = WIDTH // 2 - text_descrip_width // 2
 
 running = True
 while running:
@@ -72,9 +65,8 @@ while running:
     window.fill((BLACK))  # Preenche com a cor preta
     window.blit(background, background_rect)
     window.blit(text_intro, (centro_text_intro, 20))
-    window.blit(text_descrip, (centro_text_descrip, 85))
-    window.blit(yes_button, (centro_flappy, 200))
-    window.blit(no_button, (centro_start, 450))
+    window.blit(yes_button, (90, 504))
+    window.blit(no_button, (293, 504))
 
     pygame.display.flip()
 
